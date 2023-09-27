401c658b53630d66bafa561b79c95da99d630efd
maya9906 <mayasato99@gmail.com>
2023-09-26 20:57:47 -0700

    added javascript for hamburger and carusel

---
+++ humburger.js
@@ -0,0 +1,17 @@
+function hamburgerOpen () {
+    document.getElementById("menu-h").classList.toggle("humburger-open")
+}
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
